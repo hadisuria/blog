f9bfe3bad6d3cde608323180b68d3481348d535a
System Administrator <root@Admin.local>
2020-03-05 10:19:36 +0700

    First  commit

---
+++ src/context/BlogContextUseState.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+const BlogContextUseState = React.createContext();
+
+export const BlogProvider = ({children}) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}]);
+  };
+
+  return (
+    <BlogContextUseState.Provider value={{data: blogPosts, addBlogPost}}>
+      {children}
+    </BlogContextUseState.Provider>
+  );
+};
+
+export default BlogContextUseState;
---
+++ src/screens/CreateBlogScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const CreateBlogScreen = () => {
+  return (
+    <View>
+      <Text>Create Blog Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateBlogScreen;
